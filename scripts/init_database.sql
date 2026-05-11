0388f395445c1b7cfa534314176a2317227c16bc
Shruti_Bisht <110077886+Shruti-Bisht@users.noreply.github.com>
2026-05-11 11:10:15 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,37 @@
+/*
+====================================================================================================================
+Create Database and Schema 
+====================================================================================================================
+Script purpous : this script create a new database " DatawareHouse" 
+alse check if it already exist , if yes then database is droped and recreade . 
+along with that 3 schemas are created name : Bronze , Silver , Gold ;
+
+Warning : it will drop entire database if it already exists .
+*/
+
+-- move to main database to create new database 
+use master ;
+Go 
+
+if exists (select 1 from sys.database where name = 'DataWareHouse') 
+begin 
+ drop database DataWareHouse; 
+end ;
+Go
+
+--create ned data base
+Create database DataWareHouse; 
+Go
+
+--switch to newly created database 
+use DataWareHouse; 
+Go 
+
+--create schema
+Create Schema Bronze;
+Go
+Create Schema Silver ;
+Go
+Create Schema Gold ;
+Go
 
